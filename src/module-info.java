33847f22a6a48d9baa89042a86c95778c12f13ef
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2021-08-04 13:35:04 -0300

    Local project created

---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+module workshop {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	//exports gui;
+	//opens gui to javafx.fxml;
+	
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
