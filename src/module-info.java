974ed6eac91c42c6bf477e872be527c70cc3e793
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2021-08-14 11:54:23 -0300

    Main view controller

--- src/module-info.java
+++ src/module-info.java
@@ -2,8 +2,8 @@ module workshop {
 	requires javafx.controls;
 	requires javafx.fxml;
 	
-	//exports gui;
-	//opens gui to javafx.fxml;
+	exports gui;
+	opens gui to javafx.fxml;
 	
 	
 	opens application to javafx.graphics, javafx.fxml;
