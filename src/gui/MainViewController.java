851a0354d0f00e51b76d1a32b916b03fbdd3a163
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2021-08-15 16:07:44 -0300

    Initializing action as parameter

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -18,78 +19,62 @@ import javafx.scene.layout.VBox;
 import model.services.DepartmentService;
 
 public class MainViewController implements Initializable {
-	
+
 	@FXML
 	private MenuItem menuItemSeller;
-	
+
 	@FXML
 	private MenuItem menuItemDepartment;
-	
+
 	@FXML
-	private MenuItem menuItemAbout;	
-	
+	private MenuItem menuItemAbout;
+
 	@FXML
 	public void onMenuItemSellerAction() {
 		System.out.println("onMenuItemSellerAction");
 	}
-	
+
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
-	}
-	
-	@FXML
-	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
-	}
-	
-	@Override
-	public void initialize(URL uri, ResourceBundle rb) {		
-	}
-	
-	private synchronized void loadView(String absoluteName) {
-		try {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-		VBox newVBox = loader.load();
-		
-		//carregando a view na janela principal
-		Scene mainScene = Main.getMainScene();
-		VBox mainVBox = (VBox) ((ScrollPane)mainScene.getRoot()).getContent();
-		
-		//acessando menubar e carregando o fxml do About junto.
-		Node mainMenu = mainVBox.getChildren().get(0);
-		mainVBox.getChildren().clear();
-		mainVBox.getChildren().add(mainMenu);
-		mainVBox.getChildren().addAll(newVBox.getChildren());
-		
-		} catch (IOException e) {
-			Alerts.showAlert("IO Exception","Error loading view", e.getMessage(), AlertType.ERROR);
-		}
-	}
-	
-	private synchronized void loadView2(String absoluteName) {
-		try {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-		VBox newVBox = loader.load();
-		
-		//carregando a view na janela principal
-		Scene mainScene = Main.getMainScene();
-		VBox mainVBox = (VBox) ((ScrollPane)mainScene.getRoot()).getContent();
-		
-		//acessando menubar e carregando o fxml do About junto.
-		Node mainMenu = mainVBox.getChildren().get(0);
-		mainVBox.getChildren().clear();
-		mainVBox.getChildren().add(mainMenu);
-		mainVBox.getChildren().addAll(newVBox.getChildren());
-		
-		DepartmentListController controller = loader.getController();
-		controller.setDepartmentService(new DepartmentService());
-		controller.updateTableView();
-		
-		} catch (IOException e) {
-			Alerts.showAlert("IO Exception","Error loading view", e.getMessage(), AlertType.ERROR);
-		}
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controlle) -> {
+			controlle.setDepartmentService(new DepartmentService());
+			controlle.updateTableView();
+		});
 	}
 
+	@FXML
+	public void onMenuItemAboutAction() {
+		loadView("/gui/About.fxml", x -> {
+		});
+	}
+
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {
+	}
+
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+
+			// carregando a view na janela principal
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+
+			// acessando menubar e carregando o fxml do About junto.
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+
+			// comnado especial para ativação do Consumer vindo como parametro do metodo.
+			//esse consumer torna o metodo generico atendendo a qualquer tipo na chamada
+			T controller = loader.getController();
+			initializingAction.accept(controller);
+
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
 
 }
